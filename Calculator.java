c1568bdedb1320c7643889a1852d96c96dcaec8c
Raushan12345comp <60733038+Raushan12345comp@users.noreply.github.com>
2020-02-09 16:43:37 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+package inheritence;
+
+public class Calculator {
+    int add(int a,int b){
+
+        return (a+b);
+
+    }
+
+
+}
---
+++ FinalKey.java
@@ -0,0 +1,19 @@
+package keywords;
+
+public final class FinalKey {
+
+
+
+    final int a=10;
+
+
+
+    final void showData(){
+
+        //a=20;
+
+    }
+
+
+
+}
